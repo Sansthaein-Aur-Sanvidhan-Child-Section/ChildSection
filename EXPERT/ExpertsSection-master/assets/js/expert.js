cdc89ef30ab918b87b58fe0b64247daf1fce1a0b
Mohit yadav <ymohitayadava@gmail.com>
2024-09-09 02:08:34 +0530

    childSection

---
+++ EXPERT/ExpertsSection-master/assets/js/expert.js
@@ -0,0 +1,32 @@
+/*=============== SWIPER JS ===============*/
+let swiperCards = new Swiper(".card__content", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints:{
+      600: {
+        slidesPerView: 2,
+      },
+      968: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  // ================back button=================
+let btnBack = document.querySelector('button');
+btnBack.addEventListener('click', () => {
+    window.history.back();
+});
